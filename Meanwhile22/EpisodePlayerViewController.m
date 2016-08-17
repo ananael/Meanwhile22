ef4298fe96fea750ccd28f2c5ef9bfb056a54234
Michael Hoffman <mhoffman999@gmail.com>
2016-08-17 17:30:27 -0400

    Removed old code related to Audiometric feed.

--- Meanwhile22/EpisodePlayerViewController.m
+++ Meanwhile22/EpisodePlayerViewController.m
@@ -9,7 +9,6 @@
 #import "EpisodePlayerViewController.h"
 #import <AVKit/AVKit.h>
 #import <AVFoundation/AVFoundation.h>
-#import "MissingEpisodeSummaries.h"
 #import "PodcastParser.h"
 
 @interface EpisodePlayerViewController ()
@@ -101,8 +100,6 @@
     
     [self createImageBorderWidth:2.0 forArray:[self imageViewArray]];
     [self createButtonBorderWidth:2.0 forArray:[self buttonArray]];
-    
-    //[self showMissingSummaries];
     
     self.scrollView.showsVerticalScrollIndicator = NO;
     
@@ -386,110 +383,6 @@
     [self.player play];
     [self.avImage startAnimating];
 }
-
-#pragma mark - Additional Data
-
--(void)showMissingSummaries
-{
-    MissingEpisodeSummaries *missing = [MissingEpisodeSummaries new];
-    
-    if ([self.self.episodeTitle isEqualToString:@"Episode 52"])
-    {
-        self.episode.itunesSummary = [missing episode52];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 51"])
-    {
-        self.episode.itunesSummary = [missing episode51];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 50"])
-    {
-        self.episode.itunesSummary = [missing episode50];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 49"])
-    {
-        self.episode.itunesSummary = [missing episode49];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 47"])
-    {
-        self.episode.itunesSummary = [missing episode47];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 46"])
-    {
-        self.episode.itunesSummary = [missing episode46];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 45"])
-    {
-        self.episode.itunesSummary = [missing episode45];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 44"])
-    {
-        self.episode.itunesSummary = [missing episode44];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 43"])
-    {
-        self.episode.itunesSummary = [missing episode43];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 42"])
-    {
-        self.episode.itunesSummary = [missing episode42];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 41"])
-    {
-        self.episode.itunesSummary = [missing episode41];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 40"])
-    {
-        self.episode.itunesSummary = [missing episode40];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 39"])
-    {
-        self.episode.itunesSummary = [missing episode39];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 38"])
-    {
-        self.episode.itunesSummary = [missing episode38];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 37"])
-    {
-        self.episode.itunesSummary = [missing episode37];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 36"])
-    {
-        self.episode.itunesSummary = [missing episode36];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 35"])
-    {
-        self.episode.itunesSummary = [missing episode35];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 34"])
-    {
-        self.episode.itunesSummary = [missing episode34];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 33"])
-    {
-        self.episode.itunesSummary = [missing episode33];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 32"])
-    {
-        self.episode.itunesSummary = [missing episode32];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 31"])
-    {
-        self.episode.itunesSummary = [missing episode31];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 30"])
-    {
-        self.episode.itunesSummary = [missing episode30];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 29"])
-    {
-        self.episode.itunesSummary = [missing episode29];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 28"])
-    {
-        self.episode.itunesSummary = [missing episode28];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 27"])
-    {
-        self.episode.itunesSummary = [missing episode27];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 26"])
-    {
-        self.episode.itunesSummary = [missing episode26];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 25"])
-    {
-        self.episode.itunesSummary = [missing episode25];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 24"])
-    {
-        self.episode.itunesSummary = [missing episode24];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 23"])
-    {
-        self.episode.itunesSummary = [missing episode23];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 22"])
-    {
-        self.episode.itunesSummary = [missing episode22];
-    } else if ([self.episodeTitle isEqualToString:@"Episode 21"])
-    {
-        self.episode.itunesSummary = [missing episode21];
-    }
-    
-}
-
 
 
 @end
